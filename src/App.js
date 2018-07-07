cc3ffc2f4504309b9c7e21f3945c0619513e0eb0
David Johnston <david@blacksheepcode.com>
2018-07-07 17:41:47 +1000

    First commit

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+
+import EvalTextArea from './EvalTextArea/EvalTextArea';
+import './App.css';
+
+
+import { getEmployeeIndex } from "./Employee";
+
+const EmployeeRender = ({ employee, children }) => {
+  return (<div className="employee">
+    <p className="employee-name"> {`${employee.name} (${employee.id})`}</p>
+
+    {employee.status && <p>{employee.status}</p>}
+
+    {children}
+  </div>);
+}
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      employeeData: null,
+    };
+  }
+
+  getData = (data) => {
+    console.log(data);
+
+    this.setState({
+      employeeData: getEmployeeIndex(data)
+    });
+  }
+
+
+  renderEmployeeTree = (emp) => {
+    if (emp) return (
+
+        <EmployeeRender employee={emp}>
+          <div className="subordinates">
+            <label>Direct Reports:</label>
+            {emp.subordinates.map(e => this.renderEmployeeTree(e))}
+            {emp.subordinates.length === 0 && <span>(none)</span>}
+          </div>
+        </EmployeeRender>
+    );
+  }
+
+  renderErrorEmployees = (emps) => {
+    if (emps) return <div className="error-employees">
+      <h3>Error Employees </h3>
+      <p> The following employees had import errors: </p>
+      {emps.map((e, i) => <EmployeeRender employee={e} key={i} />)}
+    </div>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <EvalTextArea onSubmit={this.getData} />
+
+
+        <div>
+          <h3> Employees </h3>
+          {this.state.employeeData && this.renderEmployeeTree(this.state.employeeData.ceoList[0])}
+        </div>
+
+
+
+        {this.state.employeeData && this.renderErrorEmployees(this.state.employeeData.errorList)}
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/EvalTextArea/EvalTextArea.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import "./EvalTextArea.css";
+
+
+const rawData = [
+
+  { name: "Alan", id: 100, managerId: 150 },
+  { name: "Martin", id: 220, managerId: 200 },
+  { name: "Jamie", id: 150, managerId: null },
+  { name: "Alex", id: 275, managerId: 100 },
+  { name: "Bob", id: 276, managerId: 100 },
+  { name: "Charlie", id: 277, managerId: 100 },
+  { name: "Steve", id: 450, managerId: 150 },
+  { name: "David", id: 190, managerId: 400 }, 
+  { name: "Dupe1", id: 191, managerId: 100 }, 
+  { name: "Dupe2", id: 191, managerId: 100 }, 
+  { name: "Dupe3", id: 191, managerId: 100 }, 
+  { name: null, id: 193, managerId: 100 }, 
+  { name: "No-Id", id: null, managerId: 100 }, 
+
+
+
+
+];
+
+class EvalTextArea extends Component {
+
+    constructor(props) {
+      super(props); 
+
+      this.state = {
+        error: null,
+        data: JSON.stringify(rawData), 
+      }; 
+    }
+
+    onSubmit = (event) =>{
+      
+      event.preventDefault();
+
+      let data; 
+      try {
+        data = JSON.parse(this.state.data); 
+        this.setState({
+          error: null, 
+        });
+        this.props.onSubmit(data); 
+      }
+      catch(error){
+        this.setState({
+          error: error.message
+        }); 
+
+        return;
+      }
+
+    }
+
+    handleChange = (event) => {
+      this.setState({
+        data: event.target.value
+      });
+    }
+
+    render() {
+      return (
+        <form className="EvalTextArea" onSubmit = {this.onSubmit}>
+  
+            <textarea name="data" rows = {10} value = {this.state.data} onChange = {this.handleChange}/>
+            <p>{this.state.error}</p>
+
+            <button type = "submit" >submit data </button> 
+
+        </form>
+      );
+    }
+  }
+
+export default EvalTextArea; 
+
